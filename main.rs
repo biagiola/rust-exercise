7c1c982b93ebceb07762a2f7e7adb23c337ca52c
David <marcelobiagiola01@gmail.com>
2025-03-24 10:07:27 -0400

    add functions for the inserts

--- main.rs
+++ main.rs
@@ -2,54 +2,98 @@ use std::collections::HashMap; // for our pseudo tables
 mod models; // Import our pseudo models
 use models::{User, Avatar, AvatarType, UserAvatar}; // Bring structs/enum
 
-fn tbl_insert_avatar(
-    avatar_tbl: &mut HashMap<u32, Avatar>,
+fn tbl_user_insert(
+    tbl_user: &mut HashMap<u32, User>,
     id: u32,
-    avatar: Avatar
+    first_name: String,
+    last_name: String,
+    nick_name: String
 ) {
-    avatar_tbl.insert(id, avatar);
+    let user: User = User {
+        id: 1,
+        first_name: first_name,
+        last_name: last_name,
+        nick_name: nick_name
+    };
+    tbl_user.insert(id, user);
+}
+
+fn tbl_avatar_insert(
+    tbl_avatar: &mut HashMap<u32, Avatar>,
+    id: u32,
+    name: String,
+    avatar_type: AvatarType
+) {
+    let avatar: Avatar = Avatar {
+        id: id,
+        name: name,
+        avatar_type: avatar_type
+    };
+    tbl_avatar.insert(id, avatar);
+}
+
+fn tbl_user_avatar_insert(
+    tbl_user_avatar: &mut HashMap<u32, UserAvatar>,
+    id: u32,
+    user_id: u32,
+    avatar_id: u32,
+    level: u32
+) {
+    let user_avatar: UserAvatar = UserAvatar {
+        id: id,
+        user_id: user_id,
+        avatar_id: avatar_id,
+        level: level
+    };
+    tbl_user_avatar.insert(id, user_avatar);
 }
 
 fn main() {
-    // create user
-    let david_user: User = User {
-        id: 1,
-        first_name: String::from("David"),
-        last_name: String::from("Biagiola"),
-        nick_name: String::from("Deivi")
-    };
-    // create avatar
-    let gandalf_avatar: Avatar = Avatar {
-        id: 1,
-        name: String::from("Gandalf"),
-        avatar_type: AvatarType:: Magician
-    };
-    let frodo_avatar: Avatar = Avatar {
-        id: gandalf_avatar.id + 1,
-        name: String::from("Frodo"),
-        avatar_type: AvatarType::Hobbit
-    };
-    // fake save
-    let mut avatar_tbl: HashMap<u32, Avatar> = HashMap::new();
-    tbl_insert_avatar(&mut avatar_tbl, gandalf_avatar.id, gandalf_avatar);
-    tbl_insert_avatar(&mut avatar_tbl, frodo_avatar.id, frodo_avatar);
-    // println!("Insertion: {:#?}", avatar_tbl.get(&1));
+    // CREATE TABLES
+    let mut tbl_user: HashMap<u32, User> = HashMap::new();
+    let mut tbl_avatar: HashMap<u32, Avatar> = HashMap::new();
+    let mut tbl_user_avatar: HashMap<u32, UserAvatar> = HashMap::new();
 
-    // Access HasMap data: safe mode
-    // if let Some(avatar) = avatar_tbl.get(&1) {
-    //     println!("Avatar name: {}", avatar.name);
-    // }
-    // Access HasMap data: unsafe mode
-    // println!("Avatar name: {}", avatar_tbl.get(&1).unwrap().name);
+    // create user
+    tbl_user_insert(
+        &mut tbl_user,
+        1,
+        String::from("David"),
+        String::from("Biagiola"),
+        String::from("Deivi")
+    );
+
+    // create avatars
+    tbl_avatar_insert(
+        &mut tbl_avatar,
+        1,
+        String::from("Gandalf"),
+        AvatarType::Magician
+    );
+    tbl_avatar_insert(
+        &mut tbl_avatar,
+        2,
+        String::from("Frodo"),
+        AvatarType::Hobbit
+    );
+    // println!("Insertion: {:#?}", tbl_avatar.get(&1));
 
     // create userAvatar: user -> avatar
-    let david_frodo_avatar: UserAvatar = UserAvatar {
-        id: 1,
-        user_id: david_user.id,
-        avatar_id: avatar_tbl.get(&2).unwrap().id,
-        level: 20
-    };
-    println!("{:#?}", david_frodo_avatar);
+    tbl_user_avatar_insert(
+        &mut tbl_user_avatar,
+        1,
+        tbl_user.get(&1).unwrap().id,
+        tbl_avatar.get(&2).unwrap().id,
+        20
+    );
+    // println!("{:#?}", tbl_user_avatar.get(&1));
 }
 
 // TODO: make more modular fn's for saving values into the hashMap and others.
+
+// Access HasMap data: safe mode
+// if let Some(avatar) = tbl_avatar.get(&1) {
+//     println!("Avatar name: {}", avatar.name);
+// }
+// Access HasMap data: unsafe mode
+// println!("Avatar name: {}", tbl_avatar.get(&1).unwrap().name);
